5cc2e579d784e69a17520f27e8577694b77cf6ca
prathebaAT <pratheba63@gmail.com>
2024-02-02 14:01:07 +0530

    updated sqlite driver code

--- src/main/java/com/urlshortner/urlshortner/UrlShortnerApplication.java
+++ src/main/java/com/urlshortner/urlshortner/UrlShortnerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrlShortnerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 		SpringApplication.run(UrlShortnerApplication.class, args);
 	}
 
